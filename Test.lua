471f5fd53fd3915cf6f4da615f0108656caba4cd
GBMofo <calibersilent@gmail.com>
2025-07-06 22:08:25 +0800

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,4 +1,4 @@
-ocal Players = game:GetService("Players")
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local localPlayer = Players.LocalPlayer
